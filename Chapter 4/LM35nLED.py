b15de907a2f2494b4b17ee50dbbd43c7efef0a05
Seth <silver1row@cox.net>
2020-01-23 00:12:14 -0600

    Hello...I am updating this file for fun. Enjoy!
    I thought we could use a tmp36 sensor and some Fahrenheit degrees plus blinking LEDs for options.
    
    Seth

--- Chapter 4/LM35nLED.py
+++ Chapter 4/LM35nLED.py
@@ -2,8 +2,8 @@ import Adafruit_BBIO.ADC as ADC
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-RedLED = "P9_27"
-GreenLED = "P9_30"
+RedLED = "P8_19"
+GreenLED = "P8_13"
 GPIO.setup(RedLED, GPIO.OUT)
 GPIO.setup(GreenLED, GPIO.OUT)
 GPIO.output(RedLED, GPIO.LOW)
@@ -12,19 +12,25 @@ GPIO.output(GreenLED, GPIO.LOW)
 ADC.setup()
 
 try:	
-	while True:
-		reading = ADC.read('P9_40')
-		millivolts = reading * 1800  
-		temp_c = millivolts / 10
-		if (temp_c > 50):
-			print "Temperature is high"
-			GPIO.output(RedLED, GPIO.HIGH)
-			GPIO.output(GreenLED, GPIO.LOW)
-		if (temp_c < 50):
-			print "Temperature is normal"
-			GPIO.output(RedLED, GPIO.LOW)
-                        GPIO.output(GreenLED, GPIO.HIGH)
-		time.sleep(1)
+    while True:
+        reading = ADC.read('P9_40')
+        millivolts = reading * 1800  
+        temp_c = (millivolts - 500) / 10
+        temp_f = (temp_c * 9/5) + 32
+        if (temp_c > 51):
+            print("Temperature is high: %d " % temp_c)
+            GPIO.output(RedLED, GPIO.HIGH)
+            time.sleep(5)
+            GPIO.output(RedLED, GPIO.LOW)
+            time.sleep(5)
+            GPIO.output(GreenLED, GPIO.LOW)
+        if (temp_c < 50):
+            print("Temperature is normal: %d " % temp_c)
+            GPIO.output(RedLED, GPIO.LOW)
+            GPIO.output(GreenLED, GPIO.HIGH)
+        if (temp_f > 81):
+            print("The temp. is high at: %d " % temp_f)		    
+        time.sleep(2)
 
 except KeyboardInterrupt:
     print "Keyboard Interrupt"
